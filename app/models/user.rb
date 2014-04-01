0e2ffbeb60686171bcf2802c9300ea9ec084f3ec
Joel Drewlo <drewlsvern@gmail.com>
2014-04-01 11:18:27 -0500

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
 	has_many :orders
 	has_many :addresses
 
+	validates :first_name, presence: true
+	validates :last_name , presence: true
+	validates :email     , presence: true
+
 	def to_s
   	first_name + " " + last_name + " - " + id.to_s
   end

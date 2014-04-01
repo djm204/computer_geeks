03d5c246fcf8d6a7e5d2a7bcacda70f7af27c733
Joel Drewlo <drewlsvern@gmail.com>
2014-04-01 11:48:03 -0500

    modified active admin user resouce

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,10 +1,22 @@
 ActiveAdmin.register User do
 
+  config.sort_order = "first_name_asc"
+
+  index do
+    selectable_column
+    id_column
+    column :first_name
+    column :last_name
+    column :email
+    column :created_at
+    column :updated_at
+    default_actions
+  end
   
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :list, :of, :attributes, :on, :model, :first_name, :last_name, :password, :email, :active
+  permit_params :list, :of, :attributes, :on, :model, :first_name, :last_name, :password, :email, :email_confirmation
   #
   # or
   #
@@ -14,4 +26,14 @@ ActiveAdmin.register User do
   #  permitted
   # end
   
+  form do |f|
+    f.inputs "New User Details" do
+      f.input :first_name
+      f.input :last_name
+      f.input :password      
+      f.input :email
+      f.input :email_confirmation
+    end
+    f.actions
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
 	has_many :orders
 	has_many :addresses
 
-	validates :first_name, presence: true
-	validates :last_name , presence: true
-	validates :email     , presence: true
+	validates :first_name , presence: true
+	validates :last_name  , presence: true
+	validates :email, confirmation: true
+  validates :email_confirmation, presence: true
 
 	def to_s
   	first_name + " " + last_name + " - " + id.to_s

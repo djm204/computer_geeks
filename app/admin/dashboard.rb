427271b5fd84b9d832bcdd2b035869c993f624be
Joel Drewlo <drewlsvern@ubuntu-vm.(none)>
2014-03-31 22:56:30 -0500

    added provinces and tax rates to seeds.rb, ran rake db:seed to populate, edited active admin dashboard homepage, added params to active admin address.rb

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,11 +2,13 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
+  content title: proc{ "Dashboard" } do #I18n.t("active_admin.dashboard")
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+        span ('Welcome to Computer Geeks Dashboard')
+        small ('Content will be added here eventually!')
+        #span I18n.t("active_admin.dashboard_welcome.welcome")
+        #small I18n.t("active_admin.dashboard_welcome.call_to_action")
       end
     end
 

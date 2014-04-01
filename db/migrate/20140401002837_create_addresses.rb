95060f0b44c5a8f2db9902087bc0c604ac2b8109
Joel Drewlo <drewlsvern@ubuntu-vm.(none)>
2014-03-31 22:58:02 -0500

    added user reference to addresses migration

--- db/migrate/20140401002837_create_addresses.rb
+++ db/migrate/20140401002837_create_addresses.rb
@@ -6,6 +6,7 @@ class CreateAddresses < ActiveRecord::Migration
       t.string     :country
       t.string     :postal_code
       t.references :province
+      t.references :users
 
       t.timestamps
     end

1e2d5788d0b72522e1c0e31ac3b6e3f92327ecb5
Joel Drewlo <drewlsvern@ubuntu-vm.(none)>
2014-04-14 18:40:48 -0500

    modified header haml/css, modified user routes

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ ComputerGeeks::Application.routes.draw do
   #resources :contacts
   #resources :abouts
 
-  get '*unmatched_route', :to => 'index#index'
+  get '*unmatched_route', to: redirect('/index')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

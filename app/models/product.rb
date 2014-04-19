b0e5b5363f5eeb98990e6a9dcf9eb498e76158d0
Joel Drewlo <drewlsvern@gmail.com>
2014-04-18 23:11:46 -0500

    added keyword method to product model to make code more DRY

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,13 +5,16 @@ class Product < ActiveRecord::Base
   mount_uploader :productImage, ProductImageUploader
 
   def self.keyword_search(keywords)
-    keywords = '%' + keywords + '%'
-    Product.where('name LIKE ? OR description LIKE ?', keywords, keywords)
+    Product.where('name LIKE ? OR description LIKE ?', keywords(keywords), keywords(keywords))
   end
 
   def self.keyword_search_category(keywords)
+    Product.joins(:category).where('categories.name LIKE ?', keywords(keywords))
+  end
+
+  private
+
+  def self.keywords(keywords)
     keywords = '%' + keywords + '%'
-    #Category.where('name LIKE ?', keywords)    
-    Product.joins(:category).where('categories.name LIKE ?', keywords)
   end
 end

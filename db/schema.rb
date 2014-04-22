9f72e399eed5be07dcca695a3e9a14f98e6c7da4
Joel Drewlo <drewlsvern@gmail.com>
2014-04-22 00:05:53 -0500

    ran db:rake migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140411211202) do
+ActiveRecord::Schema.define(version: 20140420235951) do
 
   create_table "abouts", force: true do |t|
     t.string   "page_title"
@@ -97,6 +97,15 @@ ActiveRecord::Schema.define(version: 20140411211202) do
     t.decimal  "gst_rate"
     t.decimal  "hst_rate"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "payment_notifications", force: true do |t|
+    t.text     "params"
+    t.string   "status"
+    t.string   "transaction_id"
+    t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
